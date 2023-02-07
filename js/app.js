9e876175882afeba863d56984185dfdf3087af8c
Venegas20 <97493330+Venegas20@users.noreply.github.com>
2023-02-06 23:14:36 -0500

    guradando todos los cambios v0.1

--- js/app.js
+++ js/app.js
@@ -17,18 +17,39 @@ menuToggle.addEventListener('click', function () {
         heigth = menuCollapse.scrollHeight;
     }
     menuCollapse.style.height = `${heigth}px`;
-})
+});
 
 
-window.onload = function(){
-    document.getElementById("load").style.display="none"
+window.onload = function () {
+    document.getElementById("load").style.display = "none"
 
 };
 
+/* ===Animacion de texto=== */
+new TypeIt("#tipear", {
+    strings: ["Desarrollador Front-end"],
+//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+shuffle: false, // Alterar el orden en el que escribe las palabras.
+backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+loop: true, // Repetir el array de strings
+loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+showCursor: true, // Mostrar cursor palpitanto
+cursorChar: '|', // Caracter para el cursor
+contentType: 'html', // 'html' o 'null' para texto sin formato
+}).go();
 
-sueldos=[50000,60000,70000,80000]
-for(i=0; i<=0; i++){
-    sueldos[i]=sueldos[i]+5000
-}
-console.log(sueldos);
+/* ====ScrollReveal==== */
+// ScrollReveal({ reset: true });
+ScrollReveal().reveal('.headline');
+// ScrollReveal().reveal('.tagline', { delay: 500 });
+// ScrollReveal().reveal('.punchline', { delay: 2000 });
+
+
+
+
+
 
