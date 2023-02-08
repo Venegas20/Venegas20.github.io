aa7031889c32ae6194a60a1baba4aea390a3f4f9
Venegas20 <97493330+Venegas20@users.noreply.github.com>
2023-02-07 23:08:25 -0500

    agregando funcionabilidad con js

--- js/app.js
+++ js/app.js
@@ -19,34 +19,37 @@ menuToggle.addEventListener('click', function () {
     menuCollapse.style.height = `${heigth}px`;
 });
 
-
 window.onload = function () {
     document.getElementById("load").style.display = "none"
 
 };
 
+
+
 /* ===Animacion de texto=== */
 new TypeIt("#tipear", {
     strings: ["Desarrollador Front-end"],
-//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
-typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
-startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
-backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
-smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
-shuffle: false, // Alterar el orden en el que escribe las palabras.
-backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
-loop: true, // Repetir el array de strings
-loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
-showCursor: true, // Mostrar cursor palpitanto
-cursorChar: '|', // Caracter para el cursor
-contentType: 'html', // 'html' o 'null' para texto sin formato
+    //stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+    typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+    startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+    backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+    smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+    shuffle: false, // Alterar el orden en el que escribe las palabras.
+    backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+    loop: true, // Repetir el array de strings
+    loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+    showCursor: true, // Mostrar cursor palpitanto
+    cursorChar: '|', // Caracter para el cursor
+    contentType: 'html', // 'html' o 'null' para texto sin formato
 }).go();
 
-/* ====ScrollReveal==== */
-// ScrollReveal({ reset: true });
-ScrollReveal().reveal('.headline');
-// ScrollReveal().reveal('.tagline', { delay: 500 });
-// ScrollReveal().reveal('.punchline', { delay: 2000 });
+
+/* ===sweetalert2=== */
+let cv = document.getElementById("btnCv").addEventListener("clcik", () => {
+    Swal.fire('Opps!! El Curriculum a un no esta disponible')
+    console.log("llego hasta aqui");
+});
+
 
 
 
---
+++ js/scroll.js
@@ -0,0 +1,6 @@
+/* ====ScrollReveal==== */
+ScrollReveal({ reset: true });
+ScrollReveal().reveal('.headline');
+ScrollReveal().reveal('.pudline',{delay:250});
+ScrollReveal().reveal('.tagline', { delay: 500 });
+ScrollReveal().reveal('.punchline', { delay: 1000 });
